6976f4b323b1aef1b4a2cce9cf7421b4db28e95d
cyril <cyril@macbook-pro-de-cyril1.home>
2026-06-13 01:21:17 +0200

    Implement design system

---
+++ src/constants/images.ts
@@ -0,0 +1,5 @@
+import type { ImageSourcePropType } from "react-native";
+
+export const images = {
+  mascotLogo: require("../../assets/images/mascot-logo.png") as ImageSourcePropType,
+} as const;
